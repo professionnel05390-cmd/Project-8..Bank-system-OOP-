6efd610f8cb276fcc526cda27de31994cbea474e
ikram fullstack <professionnel05390@gmail.com>
2025-10-13 16:40:57 +0200

    Add files via upload

---
+++ Stage 9 [ UI - Manage User Menue]/show Main Menu.cpp
@@ -0,0 +1,13 @@
+// show Main Menu.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include"ClsMainScreen.h"
+using namespace std;
+int main()
+{
+   
+     clsMainScreen ::ShowMainMenue();
+    system("pause>0");
+    return 0;
+}
